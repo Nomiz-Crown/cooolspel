be9149b408a4ce1f1af36499b76efe5930f63257
novasoso <jaghatarpaj@gmail.com>
2025-03-11 14:08:33 +0100

    goober enemy follows you and jumps at you now

---
+++ Assets/Goober Enemy Stuff/scripts/GooberBehaviour.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooberBehaviour : MonoBehaviour
+{
+    Rigidbody2D rb;
+    GameObject Player;
+
+    float playerXValue;
+    float playerYValue;
+
+    [SerializeField] private float acceleration;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float JumpHeight;
+    [SerializeField] private float attackLungeRange;
+
+    private bool canReachPlayer;
+    private bool facingRight;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        facingRight = true;
+        isGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckLunge();
+        RedefineStuff();
+        CheckInRange();
+        ChasePlayer();
+        print(facingRight);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+    void CheckLunge()
+    {
+        if (canReachPlayer && isGrounded)
+        {
+            if (facingRight)
+            {
+                LungeAtRight();
+            }
+            else if (!facingRight)
+            {
+                LungeAtLeft();
+            }
+        }
+    }
+    void LungeAtRight()
+    {
+        rb.velocity += new Vector2(maxSpeed*2, 2);
+        if (isGrounded)
+        {
+            isGrounded = false;
+        }
+        print("grahh i jumped to the right");
+    }
+    void LungeAtLeft()
+    {
+        rb.velocity += new Vector2(-maxSpeed * 2, 2);
+        if (isGrounded)
+        {
+            isGrounded = false;
+        }
+        print("grahh i jumped to the left");
+    }
+    void RedefineStuff()
+    {
+        playerXValue = Player.transform.position.x;
+        playerYValue = Player.transform.position.y;
+    }
+    void CheckInRange()
+    {
+        if (transform.position.x <= playerXValue + attackLungeRange || transform.position.x >= playerXValue + attackLungeRange)
+        {
+            canReachPlayer = true;
+        }
+    }
+    void TurnToFace(string direction)
+    {
+        if (direction == "Right")
+        {
+            transform.rotation = Quaternion.identity;
+            facingRight = true;
+        }
+        else if (direction == "Left")
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+            facingRight = false;
+
+        }
+    }
+    void ChasePlayer()
+    {
+        if (transform.position.x > playerXValue + attackLungeRange)
+        {
+            canReachPlayer = false;
+            if(rb.velocity.x > -maxSpeed)
+            {
+                rb.velocity -= new Vector2(acceleration, 0);
+                TurnToFace("Left");
+            }
+        }
+        else if (transform.position.x < playerXValue - attackLungeRange)
+        {
+            canReachPlayer = false;
+            if (rb.velocity.x < maxSpeed)
+            {
+                rb.velocity += new Vector2(acceleration, 0);
+                TurnToFace("Right");
+            }
+        }
+    }
+}
