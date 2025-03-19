3a0b227b673369b809662164ff1fec18b4e7caad
dalahasten <jaghatarpaj@gmail.com>
2025-03-19 16:03:33 +0100

    wssss

---
+++ Assets/TempScoreLogic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempScoreLogic : MonoBehaviour
+{
+    public Sprite Warm;
+    public Sprite OverHeat;
+    public Sprite Cool;
+    public Sprite Cold;
+    public Sprite Heat;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
