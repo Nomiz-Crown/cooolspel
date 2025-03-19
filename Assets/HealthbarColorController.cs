9cdfb6c0e0a310da3a3748fc770bb3dd38a64db1
novasoso <jaghatarpaj@gmail.com>
2025-03-19 08:34:31 +0100

    feller

---
+++ Assets/HealthbarColorController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthbarColorController : MonoBehaviour
+{
+    public Color lowHealthColor;
+    public Color highHealthColor;
+    mchp player;
+    Image imageController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<mchp>();
+        imageController = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TransitionColor();
+    }
+    private void TransitionColor()
+    {
+        if (player != null)
+        {
+            float healthPercentage = player.hp / 100f; // Assuming hp is between 0 and 100
+            imageController.color = Color.Lerp(lowHealthColor, highHealthColor, healthPercentage);
+        }
+    }
+}
