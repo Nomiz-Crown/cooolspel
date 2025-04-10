9dcb3fb7858b6338cd7e5f14832ac3d3b31138e7
novasoso <jaghatarpaj@gmail.com>
2025-04-10 13:20:03 +0200

    scoob

--- Assets/Main character stuff/MC Scripts v.2/Fister.cs
+++ Assets/Main character stuff/MC Scripts v.2/Fister.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Fister : MonoBehaviour
@@ -163,3 +162,4 @@ public class Fister : MonoBehaviour
             }
         }
     }
+}
